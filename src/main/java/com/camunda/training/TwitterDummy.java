ff33f1b954ebd8c4dc8ed2515b7e52740a357780
Ingo Richtsmeier <ingo.richtsmeier@camunda.com>
2022-10-27 16:20:15 +0200

    improve Log and formatting

--- src/main/java/com/camunda/training/TwitterDummy.java
+++ src/main/java/com/camunda/training/TwitterDummy.java
@@ -1,12 +1,19 @@
 package com.camunda.training;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import twitter4j.TwitterException;
 
 public class TwitterDummy {
-    public void updateStatus(String content) throws TwitterException {
-        if(content.contains("duplicate")){
-            throw new TwitterException("{\"errors\":[{\"message\":\"Tweet is a duplicate\", \"code\":\"187\"}]}", new RuntimeException("oops"));
-        }
-        System.out.println("Content: " + content);
+
+  private static final Logger LOG = LoggerFactory.getLogger(TwitterDummy.class);
+
+  public void updateStatus(String content) throws TwitterException {
+    if (content.contains("duplicate")) {
+      throw new TwitterException("{\"errors\":[{\"message\":\"Tweet is a duplicate\", \"code\":\"187\"}]}",
+          new RuntimeException("oops"));
     }
+    LOG.info("Content: {}", content);
+  }
 }
